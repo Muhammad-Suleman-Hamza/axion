f9be89b60adf6b8678f6b10576f5f463ec50bec6
Muhammad Suleman Hamza <sulemanhamzatahir@gmail.com>
2026-02-25 23:53:21 +0300

    chore: removed `/api` from url for ignored urls

--- mws/__longToken.mw.js
+++ mws/__longToken.mw.js
@@ -1,4 +1,4 @@
-let ignoredURLs = ['/api/user/login', '/api/user/create'];
+let ignoredURLs = ['/user/login', '/user/create'];
 
 module.exports = ({ meta, config, managers }) => {
     return ({ req, res, next }) => {
